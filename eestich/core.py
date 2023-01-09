3560323fbd73f24bfc7ed87a89f9c2aa7804e837
Ryan Hamilton <ryangilberthamilton@gmail.com>
2023-01-09 14:45:19 -0500

    all core func implemented

--- eestich/core.py
+++ eestich/core.py
@@ -1,7 +1,7 @@
 import glob
 import os
 import re
-from typing import Dict, List
+from typing import Dict, List,Set
 
 import rasterio
 from osgeo import gdal
@@ -13,11 +13,68 @@ def get_filename(root: str, extension: str = None) -> List[str]:
     pattern = os.path.join(root, extension)
     return glob.glob(pattern)
 
+def get_basenames(paths: List[str]) -> List[str]:
+    return [os.path.basename(_) for _ in paths]
 
-def get_rows(paths: List[str], pattern: str = None):
+def get_rows(basenames: List[str], pattern: str = None) -> Set:
     pattern = '(\d+)' if pattern is None else pattern
-    basenames = [os.path.basename(_) for _ in paths]
+    return set([re.findall(pattern, filename)[-2] for filename in basenames])
 
-    for filename in basenames:
-        row = re.findall(pattern, filename)[-2]
+def files_by_row(indexs: Set[str], basenames: List[str]) -> Dict[str, str]:
+    
+    cfg = {idx: [] for idx in indexs}
+    
+    for name in basenames:
+        for idx in  indexs:
+            row = re.findall('(\d+)', name)[-2]
+            if idx == row:
+                cfg.get(row).append(name)
+    return cfg
+
+def raster_factory(buffer: list, filename: str) -> None:
+    src = buffer[-1]
         
+    mosaic, out_trans = merge(buffer)
+    
+    out_meta = src.meta.copy()
+    
+    out_meta.update({
+        'driver': "GTiff",
+        'height': mosaic.shape[1],
+        'width': mosaic.shape[2],
+        'transform': out_trans,
+        'crs': "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
+    })
+    with rasterio.open(filename, 'w', **out_meta) as rast:
+        rast.write(mosaic)
+        rast.close()
+    
+    [_.close() for _ in buffer]
+    buffer = None
+    return None
+
+def build_ras_by_row(cfg: Dict[str, List[str]], dest: str = None) -> None:
+    dest = "./dump" if dest is None else dest
+    
+    if not os.path.exists(dest):
+        os.makedirs(dest)
+    
+    for row, paths in cfg.items():
+        
+        buffer = [rasterio.open(_)for _ in paths]
+        tifname = f'dump-{row}.tif'
+        filename = os.path.join(dest, tifname)
+        raster_factory(
+            buffer=buffer,
+            filename=filename
+        )
+        tifname, filename = None, None
+    return None
+
+def build_mosaic(dump: str, outname: str) -> None:
+    
+    inputs = [os.path.join(dump, tail) for tail in os.listdir(dump)]
+    buffer = [rasterio.open(_) for _ in inputs]
+    
+    raster_factory(buffer=buffer, filename=outname)
+    return None
